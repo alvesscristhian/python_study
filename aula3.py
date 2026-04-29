f44f4f50f20cba81dbdc5bf9388a91eab3e3e875
Cristhian Alves <cristhian.ads@hotmail.com>
2026-04-29 12:30:43 -0300

    escape de strings

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Cristhian')
+print(1, 'Cristhian"')
+
+# Aspas duplas
+print("Cristhian Alves")
+print(2, "Cristhian 'Alves'")
+
+# Escape
+print("Cristhian \"Alves\"")
+
+# r
+print(r"Cristhian \"Alves\"")
