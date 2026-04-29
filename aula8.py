9b2f5d994215ab4bbc0529e8941c2dfc7fba4bf0
Cristhian Alves <cristhian.ads@hotmail.com>
2026-04-29 13:20:25 -0300

    exercicio váriaveis

---
+++ aula8.py
@@ -0,0 +1,14 @@
+nome = "Cristhian"
+sobrenome = "Alves"
+idade = 18
+ano_nascimento = 2007
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+
+
+print('Nome:', nome),
+print('Sobrenome:', sobrenome),
+print('Idade:', idade),
+print('Ano de nascimento:', ano_nascimento),
+print('É maior de idade?', maior_de_idade),
+print('Alutra em metros:', altura_metros),
