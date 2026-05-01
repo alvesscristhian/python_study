bc75561f1975cd89bf7ffaba28ba514c0abfdf60
Cristhian Alves <cristhian.ads@hotmail.com>
2026-05-01 14:38:42 -0300

    explorando lógica

--- aula20.py
+++ aula20.py
@@ -1,7 +1,11 @@
-primeiro_valor = input('Digite um valor: ')
-segundo_valor = input('Digite outro valor: ')
+primeiro_valor = input("Digite um valor: ")
+segundo_valor = input("Digite outro valor: ")
 
 if primeiro_valor > segundo_valor:
-    print(f'primeiro_valor="{primeiro_valor}" é maior que segundo_valor="{segundo_valor}"')
+    print(
+        f'primeiro_valor="{primeiro_valor}" é maior que segundo_valor="{segundo_valor}"'
+    )
 else:
-    print(f'segundo_valor="{segundo_valor}" é maior que primeiro_valor="{primeiro_valor}"')
+    print(
+        f'segundo_valor="{segundo_valor}" é maior que primeiro_valor="{primeiro_valor}"'
+    )
